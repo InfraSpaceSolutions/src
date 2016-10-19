8186253fa92b22a193a0fd9218034042013f22f8
jaigugan <gugan@infraspace.net>
2016-10-19 00:07:07 -0700

    deleted thesrc

--- api.unitethiscity.com/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="com.unitethiscity.api.WebApiApplication" Language="C#" %>
--- unitethiscity.com/App_Code/SummaryAnalytics.cs
+++
@@ -1,9 +0,0 @@
-﻿/// <summary>
-/// An analytics record
-/// </summary>
-public class SummaryAnalytics
-{
-    public string Name { get; set; }
-    public int Total { get; set; }
-    public double Percent { get; set; }
-}
